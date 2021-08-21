48c2cb16de77db42acaee80a435c3027844d7025
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-21 14:00:32 +0900

    JSONデータの取得と画面表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import axios from "axios";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
+
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data);
+    });
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todos) => (
+        <p>{todos.title}</p>
+      ))}
     </div>
   );
 }
