094ad20d28d0ac75973092da616562c6c1d6bb0a
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-21 15:08:33 +0900

    コンポーネントへの型の指定 取得するデータとstateに型を指定する。コンポーネントにはpropsの後ろに型の指定を行う。

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 import axios from "axios";
 import "./styles.css";
 import { useState } from "react";
+import { Todo } from "./Todo";
+
+//Typeの指定
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export default function App() {
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      setTodos(res.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
 
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
-      {todos.map((todos) => (
-        <p>{todos.title}</p>
+      {todos.map((todo) => (
+        <Todo
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
       ))}
     </div>
   );
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,4 +1,12 @@
-export const Todo = () => {
-  const { title, userid } = props;
-  return <p>{`${title}(ユーザー:${userid})`}</p>;
+type TodoType = {
+  userId: number;
+  title: string;
+  completed?: boolean;
+};
+
+export const Todo = (props: TodoType) => {
+  //completedはない場合もあるから　初期値としてfalseを設定する
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>;
 };
